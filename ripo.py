045e1d9f8b9622414edc76fd56fd5b02aef7c07e
hubert <259119@student.pwr.edu.pl>
2023-05-22 15:26:56 +0200

    Added detection if field is taken

--- ripo.py
+++ ripo.py
@@ -1,14 +1,23 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Wczytanie obrazu
 # image = cv2.imread('2.png')
-cap = cv2.VideoCapture('111.mp4')
+cap = cv2.VideoCapture('klip5.mp4')
 ret, frame = cap.read()
+background_color = (150, 150, 150)
 
 while True:
     # Konwersja do przestrzeni kolorów HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    hist = cv2.calcHist([gray_frame], [0], None, [256], [0, 256])
+    # plt.plot(hist)
+    # plt.show()
+    ret, thresholded_image = cv2.threshold(gray_frame, 180, 255, cv2.THRESH_BINARY)
+    cv2.imshow('gray', thresholded_image)
 
     # Określenie zakresu koloru niebieskiego
     lower_blue = np.array([90, 50, 50])
@@ -17,15 +26,32 @@ while True:
     # Binaryzacja obrazu
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
+    # Odkomentować, aby wyświetlić maskę
+    # cv2.imshow('mask', mask)
+
     # Wykrycie konturów
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Odkomentować, aby wyświetlić kontury
+    # cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+    # cv2.imshow('Contours', frame)
+
     # Iteracja przez wszystkie kontury i rysowanie prostokątów
     for contour in contours:
-        x,y,w,h = cv2.boundingRect(contour)
-        if h > 200:
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.putText(frame, "Pole odkładcze", (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+        x, y, w, h = cv2.boundingRect(contour)
+
+        # Ograniczenia testowe
+        if 150 < h < 800 and 650 > w > 350:
+            disturbance_level = np.mean(thresholded_image[y:y + h, x:x + w]) / 255
+
+            if disturbance_level > 0.01:  # Próg zakłóceń - możesz dostosować wartość według potrzeb
+                # Prostokąt czerwony
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.putText(frame, 'pole odkladcze', (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            else:
+                # Prostokąt zielony
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(frame, 'pole odkladcze', (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
     # Wyświetlenie wynikowego obrazu
     cv2.imshow("Wynik", frame)
